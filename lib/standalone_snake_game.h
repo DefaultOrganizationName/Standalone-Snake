db9e87fc283afefc092c39fda2af7dc9050d8910
kirant <kirant9797@gmail.com>
2017-07-31 17:39:31 +0300

    bug with mooving fixed

--- lib/standalone_snake_game.h
+++ lib/standalone_snake_game.h
@@ -3,11 +3,12 @@
 
 #include "screen.h"
 
-void start_snake();
-void add_snake_action(char);
-void show_points();
-int restart_option();
-void snake_menu();
-
+namespace game {
+	void start_snake();
+	void on_key_pressed(char);
+	void show_points();
+	int restart_option();
+	void snake_menu();
+}
 
 #endif
